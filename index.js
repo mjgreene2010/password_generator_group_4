918efda457182d7fcf106a0d2a9c900f45b53082
mjgreene2010 <mjgreene2010@gmail.com>
2022-03-01 18:38:52 -0600

    html structure

---
+++ index.js
@@ -0,0 +1,5 @@
+let password = document.getElementById("generated-password-box");
+let range = document.getElementById("range-value");
+let checkbox = document.querySelectorAll("checkbox");
+
+console.log(checkbox);
